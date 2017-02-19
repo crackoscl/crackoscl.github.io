df3c4544a0e9899b6a945faa599649c3db4ceb86
crackoscl <gilbert.ata@gmail.com>
2017-02-19 00:21:47 -0300

    loll

--- js/main.js
+++ js/main.js
@@ -1,48 +1,44 @@
  $(document).ready(function () {
            
 var clinica = $.getJSON("crackoscl.json", function (data) {
-    $("#pacientes tbody").empty();
+    
+$("#boton").click(Tpasientes);
+
+function Tpasientes(){
+var Prueba = $(data).filter(function (i,n){
+  return n.sexo === "F";
+});
+
+$("#pacientes tbody").empty();
     var row = "";
-    for(pacientes in data){
-        console.log(pacientes, ',', data[pacientes]);
+
+for (var i=0;i<Prueba.length;i++)
+  {
        
         row += "<tr>"+
-        "<td>"+ data[pacientes].fecha+ "</td>"+
-        "<td>"+ data[pacientes].nombre+ "</td>"+
-        "<td>"+ data[pacientes].rut + "</td>"+
-        "<td>"+ data[pacientes].comuna + "</td>"+
-        "<td>"+ data[pacientes].edad + "</td>"+
-        "<td>"+ data[pacientes].sexo + "</td>"+
-        "<td>"+ data[pacientes].pre + "</td>"+
-        "<td>"+ data[pacientes].telefono + "</td>"+
-        "<td>"+ data[pacientes].ingreso + "</td>"+
-        "<td>"+ data[pacientes].estado + "</td>"+
-    "</tr>"      
+        "<td>"+ Prueba[i].fecha+ "</td>"+
+        "<td>"+ Prueba[i].nombre+ "</td>"+
+        "<td>"+ Prueba[i].rut + "</td>"+
+        "<td>"+ Prueba[i].comuna + "</td>"+
+        "<td>"+ Prueba[i].edad + "</td>"+
+        "<td>"+ Prueba[i].sexo + "</td>"+
+        "<td>"+ Prueba[i].pre + "</td>"+
+        "<td>"+ Prueba[i].telefono + "</td>"+
+        "<td>"+ Prueba[i].ingreso + "</td>"+
+        "<td>"+ Prueba[i].estado + "</td>"+    "</tr>"  
+}
 
-    }
-      console.log(row);
+  console.log(row);
     $("#pacientes tbody").append(row);
-    row = "";            
+    row = "";
+}
+
    });
-     
-        
-  $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 100,
-      values: [ 15, 60 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-      }
-    });
-      
-    $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
-      " - " + $( "#slider-range" ).slider( "values", 1 ) );
-      
-    
-      
-  } );     
-     
+
+ 
 });
-            
+
+
+
+
+ 
