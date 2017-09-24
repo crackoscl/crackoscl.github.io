ce50c6feafb9574cbde620184b8f1fe5639efae5
crackoscl <gilbert.ata@gmail.com>
2017-09-24 13:29:33 -0300

    prueba

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
 
-new Vue({
-  el: '#app',
-  data: {
-    films: [
-      {id: 4, name: 'The Phantom Menace', seen: false},
-      {id: 5, name: 'Attack of the Clones', seen: false},
-      {id: 6, name: 'Revenge of the Sith', seen: false},
-      {id: 1, name: 'A New Hope', seen: true},
-      {id: 2, name: 'The Empire Strikes Back', seen: true},
-      {id: 3, name: 'Return of the Jedi', seen: true},
-      {id: 7, name: 'The Force Awakens', seen: false}
-    ]
-  }
-})
