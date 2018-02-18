3ccaaf7ecf39e4957dbc0c861e492b9f10d5bb34
crackoscl <gilbert.ata@gmail.com>
2018-02-18 00:28:33 -0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var baseURL = 'https://jsonplaceholder.typicode.com/users';
+var app = new Vue({
+	el: '#app',
+			created: function(){
+			this.GetBase();
+		},
 
+	data: {
+		listas:[]
+	  },
+	  methods:{
+		  GetBase: function(){
+			  axios.get(baseURL).then(response =>{
+				  this.listas = response.data
+			  });
+		  }
+	  }
+  });
+
+
+
+
+
+  
