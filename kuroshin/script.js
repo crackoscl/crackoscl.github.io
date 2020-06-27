64284eed5fbf516f6e4cd1a09c4060d253409012
crackoscl <gilbert.ata@gmail.com>
2020-06-26 21:19:37 -0400

    Add files via upload

--- kuroshin/script.js
+++ kuroshin/script.js
@@ -1,12 +1,55 @@
 M.AutoInit();
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load',() =>{
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg =>{
-        console.log('registrado con exito',reg.scope)
+;
+
+((d,w,n,c)=>{
+  //RESGITRO DE SERVICE WORKER
+  if ('serviceWorker' in n) {
+    window.addEventListener('load',() =>{
+      n.serviceWorker.register('./sw.js')
+        .then(reg =>{
+          c('registrado con exito',reg.scope)
+        })
+        .catch(err => c('a ocurrido el isguiente error',err))
+    })
+  }
+
+
+  //ACTIVANDO NOTIFICACIONES
+
+  if(window.Notification && Notification.permission !== 'denied'){
+    Notification.requestPermission(status =>{
+      console.log(status)
+      let n = new Notification('Titulo',{
+        body:'porotito nuevo producto :)',
+        icon:'./img/icon_192x192.png'
       })
-      .catch(err => console('a ocurrido el isguiente error',err))
-  })
-  
-}
+    })
+  }
+
+})(document,window, navigator,console.log);
+
+
+((d,w,n,c)=>{
+
+})(document,window, navigator,console.log);
+
+
+((d,w,n,c)=>{
+
+})(document,window, navigator,console.log);
+
+
+
+((d,w,n,c)=>{
+
+})(document,window, navigator,console.log);
+
+
+
+
+
+
+
+
+
--- kuroshin/sw.js
+++ kuroshin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-demo-edteam-cache-v1',
+const CACHE_NAME = 'Kuroshin-Prueba',
   urlsToCache = [
     './',
     './?utm=homescreen',
@@ -13,3 +13,80 @@ const CACHE_NAME = 'pwa-demo-edteam-cache-v1',
 
   ]
 
+
+
+  self.addEventListener('install', e => {
+    console.log('Evento: SW Instalado')
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          console.log('Archivos en cache')
+          return cache.addAll(urlsToCache)
+          .then( () => self.skipWaiting() )
+          //skipWaiting forza al SW a activarse
+        })
+        .catch(err => console.log('Falló registro de cache', err) )
+    )
+  })
+  
+  
+  self.addEventListener('activate', e => {
+    console.log('Evento: SW Activado')
+    const cacheWhitelist = [CACHE_NAME]
+  
+    e.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if ( cacheWhitelist.indexOf(cacheName) === -1 )
+              return caches.delete(cacheName)
+          })
+        )
+      })
+      .then(() => {
+        console.log('Cache actualizado')
+        // Le indica al SW activar el cache actual
+        return self.clients.claim()
+      })
+    )
+  })
+  
+
+
+  self.addEventListener('fetch', e => {
+    console.log('Evento: SW Recuperando')
+  
+    e.respondWith(
+      //Miramos si la petición coincide con algún elemento del cache
+      caches.match(e.request)
+      .then(res => {
+          console.log('Recuperando cache')
+          if ( res ) {
+            //Si coincide lo retornamos del cache
+            return res
+          }
+          //Sino, lo solicitamos a la red
+          return fetch(e.request)
+        })
+      )
+  })
+  
+
+  
+self.addEventListener('push', e =>{
+  console.log('evento push')
+  let title = 'Push notificacion demo',
+    options = {
+      body:'click para regresar a la aplicacion',
+      icon:'./img/icon_192x192.png',
+      vibrate:[100,50,100],
+      data:{ id: 1 },
+      actions: [
+        {'action': 'si', 'title': 'ejemplo :)' , icon:'./img/icon_192x192.png'},
+        {'action': 'si', 'title': 'no me gusta  :(' , icon:'./img/icon_192x192.png'}
+      ]
+    }
+
+    e.waitUntil(self.registration.showNotification(title, options))
+})
