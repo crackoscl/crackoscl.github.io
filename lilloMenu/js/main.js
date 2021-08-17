0bc091bac088e94fabc111abf1a503fd103a8188
crackoscl <gilbert.ata@gmail.com>
2021-08-16 21:23:07 -0400

    prueba4

--- lilloMenu/js/main.js
+++ lilloMenu/js/main.js
@@ -1,8 +1,8 @@
 M.AutoInit();
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".fixed-action-btn");
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: "left",
+    hoverEnabled: false,
   });
+});
