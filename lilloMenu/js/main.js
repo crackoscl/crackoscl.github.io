1a66aa839f5dc52336c5ef75626fd4d7927a3902
crackoscl <gilbert.ata@gmail.com>
2021-08-17 11:49:51 -0400

    prueba4

--- lilloMenu/js/main.js
+++ lilloMenu/js/main.js
@@ -1,8 +1,18 @@
 M.AutoInit();
+
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".fixed-action-btn");
   var instances = M.FloatingActionButton.init(elems, {
     direction: "left",
     hoverEnabled: false,
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var el = document.querySelector('.tabs');
+  var instance = M.Tabs.init(el, {
+    swipeable : true,
   });
 });
