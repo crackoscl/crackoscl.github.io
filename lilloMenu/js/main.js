d498b030e1c6c7de523623d7d31b2f8ea0ea120c
crackoscl <gilbert.ata@gmail.com>
2021-10-21 19:09:24 -0300

    se actualizan precios lillo carero

--- lilloMenu/js/main.js
+++ lilloMenu/js/main.js
@@ -13,26 +13,3 @@ document.addEventListener('DOMContentLoaded',function(){
     swipeable : true,
   });
 })
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.modal');
-//   var instances = M.Modal.init(elems);
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var elems = document.querySelectorAll(".fixed-action-btn");
-//   var instances = M.FloatingActionButton.init(elems, {
-//     direction: "left",
-//     hoverEnabled: false,
-//   });
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var el = document.querySelector('.tabs');
-//   var instance = M.Tabs.init(el, {
-//     swipeable : true,
-//   });
-// });
